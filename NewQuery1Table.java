964cb2ad1fcb4b91b8de6da3d63684bd073d6b89
Jenil Desai <jenil.desai25@gmail.com>
2018-12-04 16:26:04 -0600

    Add files via upload

---
+++ NewQuery1Table.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jenil
+ */
+public class NewQuery1Table {
+    String STUDENT_NAME;
+
+    public NewQuery1Table(String STUDENT_NAME) {
+        this.STUDENT_NAME = STUDENT_NAME;
+    }
+
+    public String getSTUDENT_NAME() {
+        return STUDENT_NAME;
+    }
+
+    public void setSTUDENT_NAME(String STUDENT_NAME) {
+        this.STUDENT_NAME = STUDENT_NAME;
+    }
+}
