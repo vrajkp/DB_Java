5d0977f6159247311e4bb9680f705a1193dd6cc0
Jenil Desai <jenil.desai25@gmail.com>
2018-12-04 16:25:41 -0600

    Add files via upload

---
+++ UserSelectQueryTable.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jenil
+ */
+public class UserSelectQueryTable {
+    String DATA;
+
+    public UserSelectQueryTable(String DATA) {
+        this.DATA = DATA;
+    }
+
+    public String getDATA() {
+        return DATA;
+    }
+
+    public void setDATA(String DATA) {
+        this.DATA = DATA;
+    }
+}
